1e0874c3329e2716e2b4c36d9d8eb1ffaf7eca50
Peter Walker <peterbwalker@me.com>
2013-05-09 16:35:30 -0400

    sort

--- quicksort/src/main/java/com/pbw/Quicksort.java
+++ quicksort/src/main/java/com/pbw/Quicksort.java
@@ -1,9 +1,66 @@
 package com.pbw;
 
 public class Quicksort {
-	
-	int[] sort(int[] unsorted){
-		return new int[] {0,1};
+	int[] sorted;
+
+	void sort(int[] values) {
+		this.sorted = values;
+		// Check for empty or null array
+		if (values == null || values.length == 0) {
+			return;
+		}
+		quicksort(0, values.length - 1);
 	}
-	
+
+	private void quicksort(int low, int high) {
+		System.out.println("quicksort low=" + low + " high=" + high);
+		int i = low, j = high;
+		// Get the pivot element from the middle of the list
+		int pivot = sorted[low + (high - low) / 2];
+
+		// Divide into two lists
+		while (i <= j) {
+			// If the current value from the left list is smaller then the pivot
+			// element then get the next element from the left list
+			while (sorted[i] < pivot) {
+				i++;
+			}
+			// If the current value from the right list is larger then the pivot
+			// element then get the next element from the right list
+			while (sorted[j] > pivot) {
+				j--;
+			}
+
+			// If we have found a values in the left list which is larger then
+			// the pivot element and if we have found a value in the right list
+			// which is smaller then the pivot element then we exchange the
+			// values.
+			// As we are done we can increase i and j
+			if (i <= j) {
+				swap(i, j);
+				i++;
+				j--;
+			}
+		}
+		printNumbers();
+		// Recursion
+		if (low < j)
+			quicksort(low, j);
+		if (i < high)
+			quicksort(i, high);
+	}
+
+	private void swap(int i, int j) {
+		int temp = sorted[i];
+		sorted[i] = sorted[j];
+		sorted[j] = temp;
+	}
+
+	private void printNumbers() {
+		System.out.print("numbers =");
+		for (int i = 0; i < sorted.length; ++i) {
+			System.out.print(" " + sorted[i] + " ");
+		}
+		System.out.println();
+	}
 }
--- quicksort/src/test/groovy/com/pbw/QuicksortSpec.groovy
+++ quicksort/src/test/groovy/com/pbw/QuicksortSpec.groovy
@@ -3,15 +3,21 @@ package com.pbw
 import spock.lang.*
 
 class QuicksortSpec extends Specification {
-	static Quicksort qs = new Quicksort()
+    Quicksort qs = new Quicksort()
 	
 	@Unroll
 	def "sort these arrays" (int[] unsorted, int[] sorted){
-		expect:
-		qs.sort(unsorted) == sorted
+		when:
+		qs.sort(unsorted)
+		
+		then:
+		qs.sorted == sorted
 		
 		where:
 		unsorted | sorted 
-		[1,0]	 | [0,1]  
+//		[1,0]	 | [0,1]  
+//		[ 5, 5, 6, 6, 4, 4, 5, 5, 4, 4, 6, 6, 5, 5 ] | [4,4,4,4,5,5,5,5,5,5,6,6,6,6]
+		[ 1,4,7,11,2,5,8,12,3,6,9,13] | [1,2,3,4,5,6,7,8,9,11,12,13]
+//		[]       | [] 
 	}
 }
