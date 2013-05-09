e54e94815c26ab3e370b1188946990567a9d2ad0
Peter Walker <peterbwalker@me.com>
2013-05-09 13:05:49 -0400

    done

--- count-bits/src/main/java/com/pbw/BitCounter.java
+++ count-bits/src/main/java/com/pbw/BitCounter.java
@@ -1,42 +1,43 @@
 package com.pbw;
 
 public class BitCounter {
+	BitCounter(){
+		preFill16Bitarray();
+		
+	}
+	
 	private int[] bit16Map;
 
-	static int numberOfBits(int value){
-		System.out.println("Beginning value:" + value);
+	int numberOfBits(int value){
 		int count = 0;
 	    while (value > 0) {           // until all bits are zero
 	        if ((value & 1) == 1)     // check lower bit
 	            count++;
 	        value >>= 1;              // shift bits, removing lower bit
 	    }
-		System.out.println("Finished value");
 	    return count;
 	}
 	
-	//we could use this method to prefill the array, and keep for subsequent tests.
-    void preFillarray()
+	
+    void preFill16Bitarray()
 	{
-    	bit16Map = new int[Short.MAX_VALUE];
-    	for (int i=0;i<Short.MAX_VALUE;++i){
+    	bit16Map = new int[65536];
+    	for (int i=0;i<bit16Map.length;++i){
     		bit16Map[i] = numberOfBits(i);
     	}
 	}
 	
-    static int fastBits(int value){
+     int fastBits(int value){
     	int highValue = value >> 16;
     	int lowValue = value & 0xFFFF;
-    	System.out.println( "value:" + value);
-    	System.out.println( "lowValue:" + lowValue);
-    	System.out.println( "highValue:" + highValue);
-	    return 0;
+    	int bitCount = bit16Map[lowValue] + bit16Map[highValue];
+	    return bitCount;
 	}
     
     public static void main(String [] args)
 	{
     	BitCounter bc = new BitCounter();
-    	//bc.preFillarray();
+    	bc.preFill16Bitarray();
     	bc.fastBits(555);
     	bc.fastBits(2147483647);
 
--- count-bits/src/test/groovy/com/pbw/CountBitSpec.groovy
+++ count-bits/src/test/groovy/com/pbw/CountBitSpec.groovy
@@ -3,14 +3,19 @@ package com.pbw
 import spock.lang.*
 
 class CountBitSpec extends Specification {
+	static BitCounter bc = new BitCounter()
 	
 	@Unroll
 	def "counts the bits in an integer"(int value, int bits) {
 		expect:
-		BitCounter.numberOfBits(value) == bits
-
+		bc.numberOfBits(value) == bits
+		bc.fastBits(value) == bits
+		
 		where:
 		value | bits 
+		0 | 0
+		1 | 1
+		2 | 1
 		7 | 3 
 		8 | 1 
 		9 | 2
