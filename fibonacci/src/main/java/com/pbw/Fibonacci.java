7b97918507a9b6b022650cad0399104638bcd35e
Peter Walker <peterbwalker@me.com>
2013-05-16 15:33:17 -0400

    memoize

--- fibonacci/src/main/java/com/pbw/Fibonacci.java
+++ fibonacci/src/main/java/com/pbw/Fibonacci.java
@@ -1,7 +1,14 @@
 package com.pbw;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Fibonacci {
+	private Map<Integer, Integer> cache = new HashMap<Integer, Integer>();
+
 	int[] sequence(int value) {
+		cache.put(0, 0);
+		cache.put(1, 1);
 		int[] result = new int[value + 1];
 		for (int i = 0; i <= value; ++i) {
 			result[i] = fibCalc(i);
@@ -9,12 +16,14 @@ public class Fibonacci {
 		return result;
 	}
 
-	// *TODO memoize
 	private int fibCalc(int value) {
-		if (value == 0)
-			return 0;
-		if (value == 1)
-			return 1;
-		return fibCalc(value - 1) + fibCalc(value - 2);
+		Integer cachedValue = cache.get(value);
+		if (cachedValue != null)
+			return cachedValue.intValue();
+		System.out.println("Calculating Fib for " + value);
+		int result = fibCalc(value - 1) + fibCalc(value - 2);
+		cache.put(value, result);
+		return result;
 	}
+
 }
